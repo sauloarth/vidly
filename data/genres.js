b9af303ada1c2556fef5f2f6fb6d11f5da3bba6a
Saulo Reis <sauloarth@gmail.com>
2019-01-30 17:21:07 -0200

    Simulating some data

---
+++ data/genres.js
@@ -0,0 +1,13 @@
+const genres = [{
+        id: 1,
+        genre: 'Action'
+    },
+    {
+        id: 2,
+        genre: 'Drama'
+    },
+    {
+        id: 3,
+        genre: 'Comedy'
+    },
+]
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const genres = require('./data/genres');
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello you!");
+});
 
 
 const port = process.env.PORT || 3000;
