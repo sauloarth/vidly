74efd3929b2b35e7641181a2fa8ff1f74baf850b
rbas <rbas@CSS3-1224349.saude.df.gov.br>
2019-02-05 17:28:25 -0200

    Refactoring put and post routes to genre to use database.

--- routes/genres.js
+++ routes/genres.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = express.Router();
 const genres = require('../data/genres');
 const Joi = require('joi');
-const dataDebug = require('debug')('vidly:data');
+const db = require('debug')('vidly:data');
 const mongoose = require('mongoose');
 const Genre = require('../models/genre');
 
@@ -14,16 +14,22 @@ routes.get('/', (req, res) => {
     async function sendGenres() {
         const results = await getGenres();
         res.send(results);
-        dataDebug(results);
+        db(results);
     }
 
     sendGenres();
 });
 
 routes.get('/:id', (req, res) => {
-    const data = genres.find(genre =>
-        genre.id === parseInt(req.params.id)); // req.params.id returns a string
-    data ? res.send(data) : res.status(404).send('Genre not found.');
+    const id = req.params.id;
+    db(id);
+    async function getGenreById(id) {
+        const genre = await Genre.findById(id)
+        genre ? res.send(genre) : res.status(404).send('Genre not found.');
+    }
+
+    getGenreById(id);
+
 })
 
 routes.post('/', (req, res) => {
@@ -34,34 +40,41 @@ routes.post('/', (req, res) => {
         return res.status(400).send(error.details[0].message);
     }
 
-    //once validated, genre is "persisted".
-    const newGenre = {
-        id: genres.length + 1,
-        genre: req.body.genre
-    }
+    //once validated, genre is persisted.
+    const genre = new Genre({
+        name: req.body.name
+    });
 
-    genres.push(newGenre);
-    res.send(newGenre); // returned: for the cases where the id must be known
+    async function createGenre(genre) {
+        const result = await genre.save();
+        result ? 
+        res.send(`Genre ${result.name} saved with success`) : 
+        res.status(404).send('Genre could not to be saved.');
+    }
+    createGenre(genre);
 })
 
 routes.put('/:id', (req, res) => {
-    const data = genres.find(genre =>
-        genre.id === parseInt(req.params.id)); // req.params.id returns a string
-    if (!data) return res.status(404).send('Genre not found.');
-
+    const id = req.params.id;
+    const data = req.body;
     //validating process with Joi
-    const { error } = validateGenre(req.body);
+    const { error } = validateGenre(data);
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
+    updateById(id, data);
+    async function updateById(id, data) {
+        const result = await Genre.findOneAndUpdate(id, data, {new: true})
+        if(!result) {
+            return res.status(404).send('Genre not found.');
+        }
 
-    //if values turned down to be valid the array is changed.
-    data.genre = req.body.genre;
-    res.send(data);
-
+        return res.send(result); // if values turned down to be valid the array is changed.
+    }
 })
 
 routes.delete('/:id', (req, res) => {
+
     const data = genres.find(genre =>
         genre.id === parseInt(req.params.id)); // req.params.id returns a string
     if (!data) return res.status(404).send('Genre not found');
@@ -76,7 +89,7 @@ routes.delete('/:id', (req, res) => {
 function validateGenre(genreObj) {
     // Joi schema accessible by all functions
     const schema = {
-        genre: Joi.string().min(3).required()
+        name: Joi.string().min(3).required()
     };
 
     return Joi.validate(genreObj, schema);
