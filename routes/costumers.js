46407070947c6549b2fb663a5c48709bfe275756
sauloarth <sauloarth@gmail.com>
2019-02-07 16:50:42 -0200

    Creating all basic routes for costumers.

--- routes/costumers.js
+++ routes/costumers.js
@@ -10,7 +10,14 @@ routes.get('/', async (req, res) => {
     db(results);
 })
 
-routes.post('/', async(req, res) => {
+routes.get('/:id', async (req, res) => {
+    const result = await Costumer.findById(req.params.id);
+    result ? res.send(result) : res.status(404).send('Costumer not found.');
+    // TODO: it runs into an error when an id greater
+    // or less than the regular id is passed.
+})
+
+routes.post('/', async (req, res) => {
     const { error } = validateCostumer(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
@@ -26,6 +33,32 @@ routes.post('/', async(req, res) => {
     else
         return res.status(404).send('Costumer could not be saved.');
 
+})
+
+routes.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    const { error } = validateCostumer(data);
+    if(error) {
+        //validation error
+        return res.status(400).send(error.details[0].message);
+    } 
+
+    const result = await Costumer.updateOne({_id:id}, data)
+    if(!result) {
+        return res.status(404).send('Costumer not found.');
+    } else {
+        res.send(result);
+    }
+})
+
+routes.delete('/:id', async (req, res) => {
+    const result = await Costumer.deleteOne({_id:req.params.id})
+    if(!result) {
+        return res.status(404).send('Costumer not found.');
+    }
+    return res.send(result);
 })
 
 function validateCostumer (costumer) {
