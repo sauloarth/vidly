6953d8dd84fa3e46b975c7a7b3201f5ede8e580b
sauloarth <sauloarth@gmail.com>
2019-02-07 10:15:14 -0200

    Cleaning up application's entry point.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const homeRoutes = require('./routes/home');
 const genresRoutes = require('./routes/genres');
-const startupDebug = require('debug')('vidly:startup');
+const costumersRoutes = require('./routes/costumers');
+const db = require('debug')('vidly:startup');
 const config = require('config');
 const morgan = require('morgan');
 const express = require('express');
@@ -8,14 +9,15 @@ const app = express();
 const mongoose = require('mongoose');
 
 mongoose.connect(config.get('db.path'), { useNewUrlParser: true })
-    .then(console.log('Database connected.'))
-    .catch(err => console.log('Error connecting on Database: ', err));
+    .then(db('Database connected.'))
+    .catch(err => db('Error connecting on Database: ', err));
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/genres', genresRoutes);
+app.use('/costumers', costumersRoutes);
 app.use('/', homeRoutes);
 
 app.set('view engine', 'pug');
@@ -25,8 +27,8 @@ app.set('views', './views');
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    db('Morgan enabled...');
 }
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { console.log(`vidly server running on port ${port}`) });
+app.listen(port, () => { db(`vidly server running on port ${port}`) });
