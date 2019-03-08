1039053a145198b5330b105d5a9dc665c7f682c1
Saulo Reis <sauloarth@gmail.com>
2019-03-08 18:01:03 -0300

    Setting up for integration tests.

--- startup/infra.js
+++ startup/infra.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const config = require('config');
 const winston = require('winston');
 
-module.exports = function(){
+module.exports = function() {
     mongoose.connect(config.get('db.path'), { useNewUrlParser: true })
-    .then(winston.info('New database startup.'));
+        .then(winston.info('New database startup.'));
     //Rejections will be handled by default error handling.
 }
--- startup/loggin.js
+++ startup/loggin.js
@@ -1,16 +1,16 @@
 const winston = require('winston');
 
-require('winston-mongodb');
+//require('winston-mongodb');
 require('express-async-errors');
 
-module.exports = function(){
-    winston.add(winston.transports.File, {filename: 'logfile.log'});
-    
+module.exports = function() {
+    winston.add(winston.transports.File, { filename: 'logfile.log' });
+
     //handling unhandled errors
     winston.handleExceptions(
-        new winston.transports.File({filename:'unhandled.log'})
-        )
-        
+        new winston.transports.File({ filename: 'unhandled.log' })
+    )
+
     //handling unhandled rejections
     process.on('unhandledRejection', (ex) => {
         throw ex;
