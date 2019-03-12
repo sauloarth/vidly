2fb8f6d2b1cd7413b5408de1278766da1dfd8031
Saulo Reis <sauloarth@gmail.com>
2019-03-11 21:55:01 -0300

    Fix bugs on genre route.

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const valObjId = require('../middlewares/valObjId')
 const express = require('express');
 const routes = express.Router();
 const auth = require('../middlewares/auth');
@@ -6,13 +7,14 @@ const db = require('debug')('vidly:data');
 const { Genre } = require('../models/genre');
 const { validate } = require('../models/genre');
 
-routes.get('/', async (req, res) => {
-    const results = await Genre.find();   
+
+routes.get('/', async(req, res) => {
+    const results = await Genre.find();
     res.send(results);
     db(results);
 });
 
-routes.get('/:id', async (req, res) => {
+routes.get('/:id', valObjId, async(req, res) => {
     const id = req.params.id;
     const genre = await Genre.findById(id)
     genre ? res.send(genre) : res.status(404).send('Genre not found.');
@@ -20,7 +22,7 @@ routes.get('/:id', async (req, res) => {
     // or less than the regular id is passed.
 })
 
-routes.post('/', auth, async (req, res) => {
+routes.post('/', auth, async(req, res) => {
     // validation process with Joi
     const { error } = validate(req.body);
     if (error) {
@@ -34,15 +36,15 @@ routes.post('/', auth, async (req, res) => {
 
     // trying to persist into the database
     const result = await genre.save();
-    if(result) {
-        res.send(`Genre ${result.name} saved with success`); 
+    if (result) {
+        res.send(result);
     } else {
         res.status(404).send('Genre could not be saved.');
     }
 
 })
 
-routes.put('/:id', auth, async (req, res) => {
+routes.put('/:id', auth, async(req, res) => {
     const id = req.params.id;
     const data = req.body;
 
@@ -53,27 +55,27 @@ routes.put('/:id', auth, async (req, res) => {
     }
 
     // trying to persist into the database
-    const result = await Genre.updateOne({_id:id}, data)
-    if(!result) {
+    const result = await Genre.updateOne({ _id: id }, data)
+    if (!result) {
         // DEBUG: even when document is not changed it returns an object
         return res.status(404).send('Genre not found.');
     } else {
 
-        return res.send(result); 
+        return res.send(result);
     }
 
 })
 
-routes.delete('/:id', [auth, admin], async (req, res) => {
+routes.delete('/:id', [auth, admin], async(req, res) => {
     const id = req.params.id;
 
-    const result = await Genre.deleteOne({_id:id})
-    if(!result) {
+    const result = await Genre.deleteOne({ _id: id })
+    if (!result) {
         // DEBUG: even when document is not deleted it returns an object
         return res.status(404).send('Genre not found.');
     }
 
-    return res.send(result); 
+    return res.send(result);
 })
 
 
